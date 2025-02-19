10e38522655481b056f0538ab5cee617729ad886
xqt19 <tan.xiu.qi.11@gmail.com>
2025-02-20 00:41:50 +0800

    completed till 006 ex12

--- 006_c02/ex11/ft_putstr_non_printable.c
+++ 006_c02/ex11/ft_putstr_non_printable.c
@@ -1,27 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*   ft_putstr_non_printable.c                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: xitan <xitan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/19 18:32:41 by xitan             #+#    #+#             */
-/*   Updated: 2025/02/19 18:32:41 by xitan            ###   ########.fr       */
+/*   Created: 2025/02/19 23:22:42 by xitan             #+#    #+#             */
+/*   Updated: 2025/02/19 23:22:42 by xitan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-char	ft_strcpy(char *dest, char *src)
+void	ft_putstr_non_printable(char *str)
 {
-	int	i;
+	unsigned int	i;
+	char			hex[16];
 
 	i = 0;
-	while (src[i] != '\0')
+	while (i < 16)
 	{
-		dest[i] = src[i];
+		if (i < 10)
+			hex[i] = i + '0';
+		else
+			hex[i] = (i - 10) + 'a';
 		i++;
 	}
-	dest[i] = '\0';
-	return (*dest);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= ' ' && str[i] <= '~')
+			write(1, &str[i], 1);
+		else
+		{
+			write(1, "\\", 1);
+			write(1, &hex[(str[i] >> 4) & 0x0F], 1);
+			write(1, &hex[str[i] & 0x0F], 1);
+		}
+		i++;
+	}
 }
--- 006_c02/ex12/ft_print_memory.c
+++ 006_c02/ex12/ft_print_memory.c
@@ -1,27 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*   ft_print_memory.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: xitan <xitan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/19 18:32:41 by xitan             #+#    #+#             */
-/*   Updated: 2025/02/19 18:32:41 by xitan            ###   ########.fr       */
+/*   Created: 2025/02/20 00:34:28 by xitan             #+#    #+#             */
+/*   Updated: 2025/02/20 00:34:28 by xitan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-char	ft_strcpy(char *dest, char *src)
+void	*ft_print_memory(void *addr, unsigned int size)
 {
-	int	i;
 
-	i = 0;
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (*dest);
 }
