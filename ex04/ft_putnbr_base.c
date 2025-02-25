5c64c0165fd3266a935a0c63ea6af9a1cfd8e3a3
Xiu Qi Tan <xitan@c2r6s2.42singapore.sg>
2025-02-25 13:36:19 +0800

    c04 ex03 completed

--- ex04/ft_putnbr_base.c
+++ ex04/ft_putnbr_base.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: xitan <xitan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/24 16:06:32 by xitan             #+#    #+#             */
-/*   Updated: 2025/02/25 13:25:37 by xitan            ###   ########.fr       */
+/*   Created: 2025/02/25 13:32:12 by xitan             #+#    #+#             */
+/*   Updated: 2025/02/25 13:35:21 by xitan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int	ft_atoi(char *str)
+void	ft_putnbr_base(int nbr, char *base)
 {
-	int	negative;
-	int	num;
+	// int	negative;
+	// int	num;
 
-	num = 0;
-	negative = 1;
-	while (*str == ' ')
-		str++;
-	while (*str == '+' || *str == '-')
-	{
-		if (*str == '-')
-			negative *= -1;
-		str++;
-	}
-	while (*str >= '0' && *str <= '9')
-	{
-		printf("the num is %d\n", num);
-		printf("the str is %d\n", *str);
-		num *= 10;
-		num += (*str - 48);
-		printf("the num is %d\n", num);
-		str++;
-	}
-	return (negative * num);
+	// num = 0;
+	// negative = 1;
+	// while (*str == ' ')
+	// 	str++;
+	// while (*str == '+' || *str == '-')
+	// {
+	// 	if (*str == '-')
+	// 		negative *= -1;
+	// 	str++;
+	// }
+	// while (*str >= '0' && *str <= '9')
+	// {
+	// 	printf("the num is %d\n", num);
+	// 	printf("the str is %d\n", *str);
+	// 	num *= 10;
+	// 	num += (*str - 48);
+	// 	printf("the num is %d\n", num);
+	// 	str++;
+	// }
+	// return (negative * num);
 }
 
-// int main (int argc, char *argv[]){
+int main (int argc, char *argv[])
+{
 // 	if (argc != 2) 
 // 	{
 //         printf("Invalid Input");
@@ -53,4 +54,4 @@ int	ft_atoi(char *str)
 // 	// char str3[] = "+-123 4567"; // -123
 // 	// char str4[] = ""; // 0
 // 	printf("%d\n", ft_atoi(argv[1]));
-// }
+}
