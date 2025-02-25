d55d6220c152af4f41bea0613c10ffbab6d7e387
Xiu Qi Tan <xitan@c2r6s2.42singapore.sg>
2025-02-25 13:43:20 +0800

    .

--- 009_c04/ex03/ft_atoi.c
+++ 009_c04/ex03/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: xitan <xitan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 16:06:32 by xitan             #+#    #+#             */
-/*   Updated: 2025/02/25 13:25:37 by xitan            ###   ########.fr       */
+/*   Updated: 2025/02/25 13:42:27 by xitan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,8 @@ int	ft_atoi(char *str)
 	}
 	while (*str >= '0' && *str <= '9')
 	{
-		printf("the num is %d\n", num);
-		printf("the str is %d\n", *str);
 		num *= 10;
 		num += (*str - 48);
-		printf("the num is %d\n", num);
 		str++;
 	}
 	return (negative * num);
@@ -47,10 +44,10 @@ int	ft_atoi(char *str)
 //         printf("Invalid Input");
 //         return 0;
 //     }
-// 	// char str[] = "     ---+--+1234ab567"; // -1234
-// 	// char str1[] = "     ---+- -+1234ab567"; //0 because of invalid space
-// 	// char str2[] = "     -a--+--+1234ab567"; // 0 becasue of invalid a
-// 	// char str3[] = "+-123 4567"; // -123
-// 	// char str4[] = ""; // 0
+// // 	// char str[] = "     ---+--+1234ab567"; // -1234
+// // 	// char str1[] = "     ---+- -+1234ab567"; //0 because of invalid space
+// // 	// char str2[] = "     -a--+--+1234ab567"; // 0 becasue of invalid a
+// // 	// char str3[] = "+-123 4567"; // -123
+// // 	// char str4[] = ""; // 0
 // 	printf("%d\n", ft_atoi(argv[1]));
 // }
