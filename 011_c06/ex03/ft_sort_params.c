41f8a63d187656f71f5beb169c2e71520f14f747
xqt19 <tan.xiu.qi.11@gmail.com>
2025-02-27 00:25:18 +0800

    completed c06

---
+++ 011_c06/ex03/ft_sort_params.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xitan <xitan@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/26 23:19:19 by xitan             #+#    #+#             */
+/*   Updated: 2025/02/26 23:19:19 by xitan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+
+void	ft_sort_argv(int argc, char *argv[])
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = i + 1;
+		while (j < argc)
+		{
+			if (ft_strcmp (argv[i], argv[j]) > 0)
+			{
+				str = argv[i];
+				argv[i] = argv[j];
+				argv[j] = str;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+	int	j;
+
+	if (argc < 2)
+	{
+		write(1, "Invalid Input!\n", 15);
+		return (1);
+	}
+	ft_sort_argv(argc, argv);
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			write(1, &argv[i][j], 1);
+			j++;
+		}
+		write(1, "\n", 1);
+		i++;
+	}
+}
