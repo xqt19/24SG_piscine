20f34311fbd4e143f35499a0310bdceb0cda964a
xqt19 <tan.xiu.qi.11@gmail.com>
2025-03-04 01:20:52 +0800

    starting c08

---
+++ 016_c08/ex01/ft_boolean.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xitan <xitan@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/04 00:20:59 by xitan             #+#    #+#             */
+/*   Updated: 2025/03/04 00:20:59 by xitan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN
+# define FT_BOOLEAN
+# include <unistd.h>
+
+static const char	EVEN_MSG[] = "I have an even number of arguments.";
+static const char	ODD_MSG[] = "I have an odd number of arguments.";
+static const int	SUCCESS = 0;
+
+#endif
